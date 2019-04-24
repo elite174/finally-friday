1178ae46659bda1ad99cee0b37f4b263cfa45efe
Vladislav <v-lipatov@yandex-team.ru>
2019-04-24 16:43:55 +0300

    Updates

---
+++ precache-manifest.0f5615a1100dcaf97c4e13b6ad79aced.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6a02d739ce907b19737",
+    "url": "/finally-friday/static/js/runtime~main.675690a3.js"
+  },
+  {
+    "revision": "dfec1205ee5c27b3d3ba",
+    "url": "/finally-friday/static/js/main.3db2c4a6.chunk.js"
+  },
+  {
+    "revision": "82ffec828b69ebf57c56",
+    "url": "/finally-friday/static/js/2.af5922a3.chunk.js"
+  },
+  {
+    "revision": "dfec1205ee5c27b3d3ba",
+    "url": "/finally-friday/static/css/main.5157da7a.chunk.css"
+  },
+  {
+    "revision": "acecf594e2724812227773b994b9b2e0",
+    "url": "/finally-friday/index.html"
+  }
+];
