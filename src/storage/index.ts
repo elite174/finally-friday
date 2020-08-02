ec214c5b59d755a24de250f1e3696c4269cee7e3
Vladislav <lipatov.xex@gmail.com>
2020-08-02 15:38:11 +0300

    chore: trigger redeploy

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,11 +1,11 @@
-import { IState } from "../store/typings";
-import { initialState } from "../store";
+import { IState } from '../store/typings';
+import { initialState } from '../store';
 
 const storeKey = 'ffStore$';
 
 export const saveToStorage = (store: IState) => {
     window.localStorage.setItem(storeKey, JSON.stringify(store));
-}
+};
 
 export const loadFromStorage = (): IState => {
     const storeData = window.localStorage.getItem(storeKey);
@@ -21,8 +21,14 @@ export const loadFromStorage = (): IState => {
     }
 
     return {
-        colorStore: store.colorStore ? store.colorStore : initialState.colorStore,
-        counterStore: store.counterStore ? store.counterStore : initialState.counterStore,
-        settingsStore: store.settingsStore ? store.settingsStore : initialState.settingsStore
-    }
-}
+        colorStore: store.colorStore
+            ? store.colorStore
+            : initialState.colorStore,
+        counterStore: store.counterStore
+            ? store.counterStore
+            : initialState.counterStore,
+        settingsStore: store.settingsStore
+            ? store.settingsStore
+            : initialState.settingsStore
+    };
+};
