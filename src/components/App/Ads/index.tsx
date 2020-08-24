22964ea6729e3f126edec96549d224e3f2d3c9bc
Vladislav <lipatov.xex@gmail.com>
2020-08-24 14:46:31 +0300

    chore: adding ad inside useEffect

--- src/components/App/Ads/index.tsx
+++ src/components/App/Ads/index.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './style.scss';
 
 const Ads = () => {
-    return (
-        <div className='Ads'>
-            <script
-                async
-                src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
-            ></script>
+    const adContainer = useRef<HTMLDivElement | null>(null);
 
+    useEffect(() => {
+        if (!adContainer.current) {
+            return;
+        }
+
+        const s = document.createElement('script');
+        s.src =
+            'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+        s.async = true;
+
+        adContainer.current.appendChild(s);
+
+        const s1 = document.createElement('script');
+
+        s1.innerHTML = `(adsbygoogle = window.adsbygoogle || []).push({});`;
+
+        adContainer.current.appendChild(s1);
+    }, []);
+
+    return (
+        <div className='Ads' ref={adContainer}>
             <ins
                 className='adsbygoogle'
-                style={{ display: 'block' }}
+                style={{ display: 'inline-block', height: 100, width: 300 }}
                 data-ad-client='ca-pub-6410994282681966'
                 data-ad-slot='7718000542'
-                data-ad-format='auto'
-                data-full-width-responsive='true'
             ></ins>
-            <script
-                dangerouslySetInnerHTML={{
-                    __html: `(adsbygoogle = window.adsbygoogle || []).push({});`
-                }}
-            ></script>
         </div>
     );
 };
