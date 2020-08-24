797edd5a422c7b766f72c53f846d848894186fbe
Vladislav <lipatov.xex@gmail.com>
2020-08-24 14:57:01 +0300

    chore: added two ads

--- src/components/App/Ads/index.tsx
+++ src/components/App/Ads/index.tsx
@@ -17,20 +17,36 @@ const Ads = () => {
         adContainer.current.appendChild(s);
 
         const s1 = document.createElement('script');
-
         s1.innerHTML = `(adsbygoogle = window.adsbygoogle || []).push({});`;
-
         adContainer.current.appendChild(s1);
+
+        const s2 = document.createElement('script');
+        s1.innerHTML = `(adsbygoogle = window.adsbygoogle || []).push({});`;
+        adContainer.current.appendChild(s2);
     }, []);
 
     return (
         <div className='Ads' ref={adContainer}>
-            <ins
-                className='adsbygoogle'
-                style={{ display: 'inline-block', height: 100, width: 300 }}
-                data-ad-client='ca-pub-6410994282681966'
-                data-ad-slot='7718000542'
-            ></ins>
+            <div className='Ads-Item Ads-Item_top'>
+                <ins
+                    className='adsbygoogle'
+                    style={{ display: 'block' }}
+                    data-ad-client='ca-pub-6410994282681966'
+                    data-ad-slot='7718000542'
+                    data-ad-format='auto'
+                    data-full-width-responsive='true'
+                ></ins>
+            </div>
+            <div className='Ads-Item Ads-Item_bottom'>
+                <ins
+                    className='adsbygoogle'
+                    style={{ display: 'block' }}
+                    data-ad-client='ca-pub-6410994282681966'
+                    data-ad-slot='5079969158'
+                    data-ad-format='auto'
+                    data-full-width-responsive='true'
+                ></ins>
+            </div>
         </div>
     );
 };
